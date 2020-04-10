d37572753e341a55b42b843e23a090b533d3eb1f
Andrey Potyomkin <ipad_nw@icloud.com>
2020-04-10 10:05:24 +0300

    Adds tests for useRecaptcha

---
+++ packages/useRecaptcha/src/__tests__/useRecaptcha.test.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { renderHook } from '@testing-library/react-hooks';
+
+import useRecaptcha from '../useRecaptcha';
+
+describe('useRecaptcha', () => {
+  const siteKey = '4becac2b-f363-4a97-a289-ad505c3b1636';
+
+  jest.mock('react-script-hook');
+
+  it('the default status is set correctly', () => {
+    const { result } = renderHook(() => useRecaptcha({ siteKey }));
+    const { readinessStatus } = result.current;
+
+    const expectedDefaultStatus = {
+      loading: true,
+      ready: false,
+      error: null,
+    };
+
+    expect(readinessStatus).toEqual(expectedDefaultStatus);
+  });
+});
